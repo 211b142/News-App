bfb22d52d8d311b9d1f74f55b62fc321528f29d7
myusername <ishasharma9843@gmail.com>
2023-05-10 20:10:55 +0530

    Files updated

--- app/src/main/java/com/example/newsapp1/RetrofitAPI.java
+++ app/src/main/java/com/example/newsapp1/RetrofitAPI.java
@@ -4,7 +4,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Url;
 
-public interface retrofitAPI {
+public interface RetrofitAPI {
     @GET
     Call<NewsModal> getAllNews( @Url String url);
     @GET
